7bc90cd69dbd3066d8024cfd216a53da2cc5b930
tomsontai <ktai8@my.bcit.ca>
2019-09-28 19:39:28 -0700

    array not functioning correctly

--- style/lab3.js
+++ style/lab3.js
@@ -1,4 +1,4 @@
-var artistArray = [];
+const artistArray = [];
 
 function add_Artist_Form() {
     var x = document.getElementById("inputForm");
@@ -37,7 +37,56 @@ function addToDatabase() {
     }
 }
 
-function showInfo() {
+function showAllArtists() {
+    if (artistArray.length != 0) {
+        for (i = 0; i < artistArray.length; i++) {
+            var artistClass = document.createElement('div');
+        
+
+            var artistImg = document.createElement('div');
+            artistImg.setAttribute('class', 'artistImage');
+            var imageSrc = document.createElement('img');
+            imageSrc.src = artistArray[i][2].value;
+            artistImg.appendChild(imageSrc);
+
+
+            artistClass.appendChild(artistImg);
+
+            var artistInfoClass = document.createElement('div');
+            var artistTitleClass = document.createElement('div');
+            var artistDescClass = document.createElement('div');
+            
+            artistInfoClass.setAttribute('class', 'artistInfo');
+            artistTitleClass.setAttribute('class', 'artistTitle');
+            artistDescClass.setAttribute('class', 'artistDesc');
+
+            artistTitleClass.textContent = artistArray[i][0].value;
+            artistInfoClass.textContent = artistArray[i][1].value;
+
+            artistInfoClass.appendChild(artistTitleClass);
+            artistInfoClass.appendChild(artistDescClass);
+
+            artistClass.appendChild(artistInfoClass);
+
+            var deleteButtonClass = document.createElement("INPUT");
+            deleteButtonClass.setAttribute("type", "button");
+            deleteButtonClass.setAttribute("value", "Delete");
+            deleteButtonClass.setAttribute("id", "delete" + artistArray.length);
+            deleteButtonClass.setAttribute("onclick", "deleteFunction(this)");
+            deleteButtonClass.setAttribute("name", artistArray.length - 1)
+            
+            //deleteButtonClass.addEventListener('onClick', 'deleteFunction()');
+            artistClass.appendChild(deleteButtonClass);
+
+            var ulElementId = document.getElementById("ulElement");
+            ulElementId.appendChild(artistClass);
+        }
+
+    }
+
+}
+
+function showInfo() { // Displays the artist being added to the array.
     let artistText = document.getElementById("inputArtistText");
     let aboutText = document.getElementById("inputAboutText");
     let urlText = document.getElementById("inputURLText");
@@ -53,7 +102,7 @@ function showInfo() {
         var artistImg = document.createElement('div');
         artistImg.setAttribute('class', 'artistImage');
         var imageSrc = document.createElement('img');
-        imageSrc.src = artistArray[0][2].value;
+        imageSrc.src = artistArray[artistArray.length - 1][2].value;
         artistImg.appendChild(imageSrc);
 
 
@@ -67,8 +116,8 @@ function showInfo() {
         artistTitleClass.setAttribute('class', 'artistTitle');
         artistDescClass.setAttribute('class', 'artistDesc');
 
-        artistTitleClass.textContent = artistArray[0][0].value;
-        artistInfoClass.textContent = artistArray[0][1].value;
+        artistTitleClass.textContent = artistArray[artistArray.length - 1][0].value;
+        artistInfoClass.textContent = artistArray[artistArray.length - 1][1].value;
 
         artistInfoClass.appendChild(artistTitleClass);
         artistInfoClass.appendChild(artistDescClass);
@@ -78,17 +127,20 @@ function showInfo() {
         var deleteButtonClass = document.createElement("INPUT");
         deleteButtonClass.setAttribute("type", "button");
         deleteButtonClass.setAttribute("value", "Delete");
-        deleteButtonClass.setAttribute("id", "deleteButton");
-
-
+        deleteButtonClass.setAttribute("id", "delete" + artistArray.length);
+        deleteButtonClass.setAttribute("onclick", "deleteFunction(this)");
+        deleteButtonClass.setAttribute("name", artistArray.length - 1)
+        
+        //deleteButtonClass.addEventListener('onClick', 'deleteFunction()');
         artistClass.appendChild(deleteButtonClass);
 
         var ulElementId = document.getElementById("ulElement");
         ulElementId.appendChild(artistClass);
 
-
-        console.log("artistimg = " + artistImg);
-        console.log("imageSrc = " + imageSrc);
+        // console.log("delete button id = " + deleteButtonClass.id);
+        console.log("name = " + deleteButtonClass.name);
+        // console.log("artistimg = " + artistImg);
+        // console.log("imageSrc = " + imageSrc);
 //         var div = document.createElement('div');
 // div.textContent = "Sup, y'all?";
 // div.setAttribute('class', 'note');
@@ -97,7 +149,33 @@ function showInfo() {
     }
 }
 
-function deleteFunction() {
-    var x = document.getElementById("ulElement");
-    x.remove(x.selectedIndex);
+function deleteFunction(obj) {
+    console.log("delete function is called");
+    var x = document.getElementById("delete");
+    var y = document.getElementById("ulElement");
+    
+    console.log("Object is " + obj.name);
+
+    //if (this) {
+        
+       // console.log("what's this? " + x.name);
+        artistArray.splice(parseInt(obj.name, 10), 1);
+        y.remove();
+    //}
+    var z = document.getElementById("baseDiv");
+
+    if (!document.getElementById("ulElement")) {
+        var zz = document.createElement("ul");
+        zz.setAttribute("id", "ulElement");
+
+        z.appendChild(zz);
+
+    }
+    //var x = document.getElementById("ulElement");
+    //x.remove(x.selectedIndex);
+
+    // var y = document.getElementById("delete");
+    // console.log(y.id);
+    showAllArtists();
+
 }
