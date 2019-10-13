17d112d1bd6fe02edda2ead81119a974544a5675
tomsontai <ktai8@my.bcit.ca>
2019-10-12 18:59:59 -0700

    restructured lab5 files

--- COMP4711/Lab5/expressapp/index.js
+++ COMP4711/Lab5/expressapp/index.js
@@ -5,10 +5,30 @@ var fs = require("fs");  
 var bodyParser = require('body-parser');
 var multer = require('multer');
 
+app.set('port', process.env.PORT || 3000);
+
+// app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(multer({ dest: '/tmp/'}));
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/'}));
+app.get('../Lab5.html', function (req, res) {
+   res.sendFile( __dirname + "../" + "index.html" );
+})
 
-app.get('/', (req, res) => res.send('Hello World!'))  
-app.listen(3000, () => console.log('Server ready'))  
+// app.get('/', (req, res) => res.send('Hello World!'))  
+// app.listen(3000, () => console.log('Server ready'))  
 
+app.post('/process_post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        inputArtistName:req.body.inputArtistName,
+        inputAbout:req.body.inputAbout,
+        inputURL:req.body.inputURL
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+ })
