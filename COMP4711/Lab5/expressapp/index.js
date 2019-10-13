f8e90a0bf7d7aa63bece0c3072339957a9defd88
tomsontai <ktai8@my.bcit.ca>
2019-10-12 22:07:59 -0700

    something showed up on localhost

--- COMP4711/Lab5/expressapp/index.js
+++ COMP4711/Lab5/expressapp/index.js
@@ -5,7 +5,17 @@ var fs = require("fs");  
 var bodyParser = require('body-parser');
 var multer = require('multer');
 
-app.set('port', process.env.PORT || 3000);
+const expressHbs = require('express-handlebars');
+app.engine(
+    'hbs',
+    expressHbs({
+      layoutsDir: 'views/layouts/',
+      defaultLayout: 'main',
+      extname: 'hbs'
+    })
+  );
+  app.set('view engine', 'hbs');
+  app.set('views', 'views');
 
 // app.use(express.static('public'));
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,6 +28,10 @@ app.use(express.static('public'));
 app.get('../Lab5.html', function (req, res) {
    res.sendFile( __dirname + "../" + "index.html" );
 })
+
+app.get('/', function (req,res) {
+    res.render('home', { pageTitle: 'People App', heading: 'Welcome to People App'});
+});
 
 // app.get('/', (req, res) => res.send('Hello World!'))  
 // app.listen(3000, () => console.log('Server ready'))  
@@ -32,3 +46,29 @@ app.post('/process_post', urlencodedParser, function (req, res) {
     console.log(response);
     res.end(JSON.stringify(response));
  })
+
+// Defines a custom 404 Page and we use app.use because
+// the request didn't match a route (Must follow the routes)
+app.use(function(req, res) {
+    // Define the content type
+    res.type('text/html');
+   
+    // The default status is 200
+    res.status(404);
+   
+    // Point at the 404.handlebars view
+    res.render('404');
+});
+
+
+  // Custom 500 Page
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+
+    // Point at the 500.handlebars view
+    res.render('partials/500');
+});
+
+
+ app.listen(3000, () => console.log('Server ready'));
